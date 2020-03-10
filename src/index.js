b4eeba0599032c2eedb7ac5437daf6a89e4d1c6a
Victoria Antonova <gershovavictoria@gmail.com>
2020-03-10 13:22:37 +0100

    relate version option to package.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import commander from 'commander';
+import pjson from '../package.json';
 
 export default () => {
+  const progVersion = pjson.version;
   const program = new commander.Command();
   program
-    .version('0.0.1')
+    .version(progVersion)
     .description('Compares two configuration files and shows a difference')
     .parse(process.argv);
 };
