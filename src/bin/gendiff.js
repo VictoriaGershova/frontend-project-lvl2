8df1107cdcfccfc65164730922fb27e5b83a2f6a
Victoria Antonova <gershovavictoria@gmail.com>
2020-03-15 17:01:28 +0100

    creat gendiff function in index.js and set as action in gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
-import genDiff from '..';
+import commander from 'commander';
+import genDiff, { getVersion } from '..';
 
-genDiff();
+const progVersion = getVersion();
+const program = new commander.Command();
+program
+  .version(progVersion)
+  .description('Compares two configuration files and shows a difference')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(
+    (firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)),
+  )
+  .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import commander from 'commander';
+import fs from 'fs';
+import _ from 'lodash';
 import pjson from '../package.json';
 
-export default () => {
-  const progVersion = pjson.version;
-  const program = new commander.Command();
-  program
-    .version(progVersion)
-    .description('Compares two configuration files and shows a difference')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<firstConfig> <secondConfig>')
-    .action(() => {})
-    .parse(process.argv);
+// return JSON file data transformed to object by path
+const getFileData = (pathToFile) => {
+  const fileData = fs.readFileSync(pathToFile);
+  return JSON.parse(fileData);
 };
+
+// generate difference of two objects
+// return string
+const getDataDiff = (obj1, obj2) => {
+  const ar1 = Object.entries(obj1);
+  const ar2 = Object.entries(obj2);
+  const deleted = _.differenceWith(ar1, ar2, _.isEqual).map((p) => [...p, '-']);
+  const added = _.differenceWith(ar2, ar1, _.isEqual).map((p) => [...p, '+']);
+  const same = _.intersectionWith(ar1, ar2, _.isEqual).map((p) => [...p, '-']);
+  const diff = [...deleted, ...added, ...same]
+    .sort()
+    .map(([k, v, sign]) => `\t${sign} ${k}: ${v}\n`)
+    .join('');
+  return `{\n${diff}\n}`;
+};
+
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const diff = getDataDiff(
+    getFileData(pathToFile1),
+    getFileData(pathToFile2),
+  );
+  return diff;
+};
+
+export const getVersion = () => pjson.version;
+export default genDiff;
