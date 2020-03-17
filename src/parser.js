bb597ad618ab3144879364704f4e9b49e37ab29f
Victoria Antonova <gershovavictoria@gmail.com>
2020-03-17 15:37:46 +0100

    add parser func for ini format with new lib

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parserFunctions = {
   json: JSON.parse,
   yml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
 export default (extName) => (data = {}) => parserFunctions[extName](data);
