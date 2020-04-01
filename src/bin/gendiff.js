78791be3349d444544da76f52f999ad0f0e3aab9
Victoria Antonova <gershovavictoria@gmail.com>
2020-04-01 23:51:12 +0200

    add passing option format to gendiff function in bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -11,6 +11,6 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
   .action(
-    (firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)),
+    (firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig, program.format)),
   )
   .parse(process.argv);
