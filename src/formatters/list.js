f10e20a2decdae036eac7153616e7baf85d3ef4c
Victoria Antonova <gershovavictoria@gmail.com>
2020-04-14 23:17:02 +0200

    replace marge for objects to stringify

--- src/formatters/list.js
+++ src/formatters/list.js
@@ -3,19 +3,20 @@ import states from '../state';
 
 const tabLength = 4;
 
-const stringifyObject = (obj, depth = 0) => {
+const stringifyObject = (obj, depth) => {
+  const marge = tabLength * (depth + 1);
   const lines = Object.keys(obj)
     .map((key) => {
       const v = obj[key];
       const strV = v instanceof Object ? stringifyObject(v, depth + 1) : v;
-      return `${' '.repeat(tabLength * (depth + 1))}${key}: ${strV}`;
+      return `${' '.repeat(marge)}${key}: ${strV}`;
     });
-  return ['{', ...lines, `${' '.repeat(tabLength * depth)}}`].join('\n');
+  return ['{', ...lines, `${' '.repeat(depth * tabLength)}}`].join('\n');
 };
 
-const stringifyValue = (value) => {
+const stringifyValue = (value, depth) => {
   if (value instanceof Object) {
-    return stringifyObject(value);
+    return stringifyObject(value, depth);
   }
   return `${value}`;
 };
@@ -31,27 +32,25 @@ export default (diff) => {
         hasInnerChanges,
         children,
       } = propertyDiff();
-      const linesTemplate = (stateSign, contentLines, marge) => ([
+      const linesTemplate = (stateSign, contentLines) => ([
         `${`${stateSign} `.padStart(margeWidth, ' ')}${property}: ${contentLines[0]}`,
-        ...contentLines
-          .filter((line, index) => index !== 0)
-          .map((line) => `${' '.repeat(marge)}${line}`),
+        ...contentLines.filter((line, index) => index !== 0),
       ]);
       switch (state) {
         case states.added:
-          return linesTemplate('+', stringifyValue(value).split('\n'), margeWidth);
+          return linesTemplate('+', stringifyValue(value, depth + 1).split('\n'));
         case states.deleted:
-          return linesTemplate('-', stringifyValue(value).split('\n'), margeWidth);
+          return linesTemplate('-', stringifyValue(value, depth + 1).split('\n'));
         case states.changed:
           if (hasInnerChanges) {
-            return linesTemplate(' ', formatDiff(children, depth + 1), 0);
+            return linesTemplate(' ', formatDiff(children, depth + 1));
           }
           return [
-            ...linesTemplate('-', stringifyValue(value.oldValue).split('\n'), margeWidth),
-            ...linesTemplate('+', stringifyValue(value.newValue).split('\n'), margeWidth),
+            ...linesTemplate('-', stringifyValue(value.oldValue, depth + 1).split('\n')),
+            ...linesTemplate('+', stringifyValue(value.newValue, depth + 1).split('\n')),
           ];
         default:
-          return linesTemplate(' ', stringifyValue(value).split('\n'), margeWidth);
+          return linesTemplate(' ', stringifyValue(value, depth + 1).split('\n'));
       }
     });
     return ['{', ...flatten(lines), `${' '.repeat(tabLength * depth)}}`];
