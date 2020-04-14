87bfa038445dc1ebe3ed640aa2938df71587b47c
Victoria Antonova <gershovavictoria@gmail.com>
2020-04-15 00:07:47 +0200

    change JSON

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,38 +1,31 @@
 export default (diff) => {
   const formatDiff = (diffItems) => {
-    const lines = diffItems.reduce(
-      (acc, propertyDiff) => {
-        const {
+    const lines = diffItems.map((propertyDiff) => {
+      const {
+        property,
+        state,
+        value,
+        hasInnerChanges,
+        children,
+      } = propertyDiff();
+      if (hasInnerChanges) {
+        return {
           property,
           state,
           value,
           hasInnerChanges,
-          children,
-        } = propertyDiff();
-        if (hasInnerChanges) {
-          return {
-            ...acc,
-            [property]: {
-              state,
-              value,
-              hasInnerChanges,
-              children: formatDiff(children),
-            },
-          };
-        }
-        return {
-          ...acc,
-          [property]: {
-            state,
-            value,
-          },
+          children: formatDiff(children),
         };
-      },
-      {},
-    );
+      }
+      return {
+        property,
+        state,
+        value,
+      };
+    });
     return lines;
   };
-  const lines = formatDiff(diff);
+  const lines = { properties: formatDiff(diff) };
   const result = JSON.stringify(lines, null, 2);
   return result;
 };
